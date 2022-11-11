41cf8e65353447f63daff80f72fbe8a6c39a51b7
Trung Hieu Do <hieu.yl.ptit1312@gmail.com>
2022-11-12 00:30:08 +0700

    Base

---
+++ my-app/src/pages/home/home.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+      <div>
+        <div>HỆ THỐNG IOT NHẬN DIỆN RÁC THẢI</div>
+        <div>From team 15 with love</div>
+      </div>
+      <div>
+        <div>
+          Login
+        </div>
+        <div style={{display:'flex'}}>
+          <div>
+            Statistical
+          </div>
+          <div>
+            Quantity & Ratio
+          </div>
+        </div>
+        <div>
+          Garbage image
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default home
